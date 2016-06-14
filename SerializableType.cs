ac4990eb5aab169e063688468e8acdcd14c50255
Wim de With <nauxuron@wimdewith.com>
2016-06-14 22:06:24 +0200

    Change type from string to enum

---
+++ SerializableType.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file = "SerializableType.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace SerializedObjects
+{
+    /// <summary>
+    /// Contains the type of the serialized object.
+    /// </summary>
+    public enum SerializableType
+    {
+        /// <summary>
+        /// The building type.
+        /// </summary>
+        Building,
+
+        /// <summary>
+        /// The car type.
+        /// </summary>
+        Car,
+
+        /// <summary>
+        /// The garden type.
+        /// </summary>
+        Garden,
+
+        /// <summary>
+        /// The bench type.
+        /// </summary>
+        Bench,
+
+        /// <summary>
+        /// The television type.
+        /// </summary>
+        Television
+    }
+}
